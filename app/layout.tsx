3e793455625dd371f12636018a9f7a0bf9587eac
Sam W <sam@sammymayhem.com>
2023-04-24 12:39:49 -0700

    Finished styling and setting up navbar components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Nunito } from 'next/font/google';
+import Navbar from './components/navbar/Navbar';
 
 export const metadata = {
   title: 'Airbnb Clone',
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   )
 }
