aef1ee788f9045824aab290a3495a7e4a1660866
Sam W <sam@sammymayhem.com>
2023-04-25 17:30:17 -0700

    Added GitHub OAuth and importing of GitHub Avatar

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,11 +1,16 @@
 'use client'
 
 import Image from "next/image";
+interface AvatarProps {
+  src: string | null | undefined
+};
 
-const Avatar = () => {
+const Avatar: React.FC<AvatarProps> = ({
+  src
+}) => {
   return (
     <div>
-      <Image className="rounded-full" height="30" width="30" alt="Avatar" src="/images/placeholder.jpg" />
+      <Image className="rounded-full" height="30" width="30" alt="Avatar" src={src || "/images/placeholder.jpg"} />
     </div>
   )
 }
--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -12,6 +12,7 @@ import Heading from '../Heading';
 import Input from '../inputs/Input';
 import { toast } from 'react-hot-toast';
 import Button from '../Button';
+import { signIn } from 'next-auth/react';
 
 const RegisterModal = () => {
 
@@ -80,7 +81,7 @@ const RegisterModal = () => {
         <div className='flex flex-col gap-4 mt-3'>
             <hr />
             <Button outline label='Continue with Google' icon={FcGoogle} onClick={() => {}}/>
-            <Button outline label='Continue with Github' icon={AiFillGithub} onClick={() => {}}/>
+            <Button outline label='Continue with Github' icon={AiFillGithub} onClick={() => signIn('github')}/>
             <div className='text-neutral-500 text-center mt-4 font-light'>
                 <div className='justify-center flex flex-row items-center gap-2'>
                     <div>
