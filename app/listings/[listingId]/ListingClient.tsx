f92538dae31ac0d149a0bbe47c312874b494cbb1
Sam W <sam@sammymayhem.com>
2023-05-01 13:58:51 -0700

    Finished the ListingHead component to show individual listings when one is clicked

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,5 +1,12 @@
+'use client';
+
+import Container from "@/app/components/Container";
+import { categories } from "@/app/components/Navbar/Categories";
 import { SafeListing, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client"
+import { useMemo } from "react";
+
+import ListingHead from "@/app/components/listings/ListingHead";
 
 
 interface ListingClientProps {
@@ -14,11 +21,27 @@ const ListingClient: React.FC<ListingClientProps> = ({
     listing,
     currentUser
 }) => {
-  return (
-    <div>
-      Listing Client
-    </div>
-  )
+
+    // This allows us to access the categories array and match the label with the category 
+    const category = useMemo(() => {
+        return categories.find((item) => item.label === listing.category);
+    }, [listing.category])
+
+    return (
+        <Container>
+            <div className="max-w-screen-lg mx-auto">
+                <div className="flex flex-col gap-6">
+                    <ListingHead 
+                        title={listing.title}
+                        imageSrc={listing.imageSrc}
+                        locationValue={listing.locationValue}
+                        id={listing.id}
+                        currentUser={currentUser}
+                    />
+                </div>
+            </div>
+        </Container>
+    )
 }
 
 export default ListingClient;
