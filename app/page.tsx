7b68df4bcb71b77dcdaac452587bf87ccfc2e5eb
Sam W <sam@sammymayhem.com>
2023-05-03 12:43:29 -0700

    Trying to fix Vercel error when loading build. Changed IListingsParams path in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import getCurrentUser from "./actions/getCurrentUser";
-import getListings, { IListingsParams } from "./actions/getListings";
+import getListings, { IListingsParams } from "@/app/actions/getListings";
 
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
